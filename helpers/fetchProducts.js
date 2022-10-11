7195d040e886d9a3a52864f428c3360516335b8c
Jefferson Santos da Silva <jeffersonadm.rn@hotmail.com>
2022-10-11 17:03:37 -0300

    adicionado requisito 1 do projeto

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,11 @@
-const fetchProducts = () => {
+const fetchProducts = async (produto) => {
   // seu código aqui
+    if (!produto) {
+      throw new Error('You must provide an url');
+    }
+    const resposta = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+    const json = await resposta.json();
+    return json;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,22 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Teste se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  test('Teste se o fetch foi chamado', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  test('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint correto', () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+  test('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    const resposta = await fetchProducts('computador');
+    expect(resposta).toEqual(computadorSearch);
+  });
+  test('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: "You must provide an url"', async () => {
+    expect(fetchProducts()).rejects.toThrow('You must provide an url');
+  });
 });
