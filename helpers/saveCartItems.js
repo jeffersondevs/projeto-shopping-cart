a38936039126852a7fe8605edbaecfb95a354d99
Jefferson Santos <jeffersonadm.rn@hotmail.com>
2023-04-30 12:48:03 -0300

    refatoração geral do código e implementando algumas funcionalidades como também a inclusão de propriedades de css para atender a funcionalidades da aplicação

--- helpers/saveCartItems.js
+++
@@ -1,8 +0,0 @@
-const saveCartItems = (items) => {
-  // seu código aqui
-  localStorage.setItem('cartItems', items);
-};
-
-if (typeof module !== 'undefined') {
-  module.exports = saveCartItems;
-}
--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-import { 
-  fetchCategories, 
-  fetchProductsByCategory, 
+import {
+  fetchCategories,
+  fetchProductsByCategory,
   fetchProdutcsByQuery,
 } from './helpers/mercadoLivreAPI.js';
 import { fetchItem } from './helpers/fetchItem.js';
+import {
+  getSavedCartItems,
+  saveCartItems,
+} from './helpers/localStorageHandlers.js';
 
-const btnVazio = document.querySelector('.empty-cart');
-const cartLista = document.querySelector('.cart__items');
-const precoTotalDiv = document.querySelector('.total-price');
-const carregandoContainer = document.querySelector('.carregandoContainer');
-const selectCategories = document.querySelector('.categories-list');
-const emptyMessage = document.querySelector('.product-list-empty');
-const productList = document.querySelector('.items');
-const sectionItens = document.querySelector('.items');
-const formPesquisa = document.querySelector('.section-pesquisa');
-const sortOrderSelect = document.querySelector('.sort-order');
+const DOM = {
+  btnVazio: document.querySelector('.empty-cart'),
+  cartLista: document.querySelector('.cart__items'),
+  precoTotalDiv: document.querySelector('.total-price'),
+  carregandoContainer: document.querySelector('.carregandoContainer'),
+  selectCategories: document.querySelector('.categories-list'),
+  emptyMessage: document.querySelector('.product-list-empty'),
+  productList: document.querySelector('.items'),
+  sectionItens: document.querySelector('.items'),
+  formPesquisa: document.querySelector('.section-pesquisa'),
+  sortOrderSelect: document.querySelector('.sort-order'),
+};
+
+const toggleLoading = (isLoading) => {
+  if (isLoading) {
+    DOM.carregandoContainer.classList.remove('hide');
+  } else {
+    DOM.carregandoContainer.classList.add('hide');
+  }
+};
+
+const toggleEmptyMessage = (isEmpty) => {
+  DOM.emptyMessage.style.display = isEmpty ? 'block' : 'none';
+};
 
 const sortProducts = (products, sortOrder) => {
   if (sortOrder === 'price_asc') {
@@ -25,236 +43,276 @@ const sortProducts = (products, sortOrder) => {
   return products;
 };
 
-sortOrderSelect.addEventListener('change', async (event) => {
-  const sortOrder = event.target.value;
+const clearProductList = () => {
+  DOM.productList.innerHTML = '';
+  DOM.productList.insertAdjacentHTML(
+    'beforeend',
+    '<div class="empty-message">Nenhum produto encontrado.</div>'
+  );
+};
 
-  // Obter a lista atual de produtos (por categoria ou pesquisa)
-  const currentCategory = selectCategories.value;
-  const currentQuery = document.querySelector('#pesquisaInput').value;
+const checkProductList = () => {
+  toggleEmptyMessage(!DOM.productList || DOM.productList.children.length === 0);
+};
 
-  let products;
-  if (currentCategory) {
-    const { results } = await fetchProductsByCategory(currentCategory);
+const updateProductList = async (category = null, query = null) => {
+  let products = [];
+  toggleLoading(true);
+
+  if (category) {
+    const { results } = await fetchProductsByCategory(category);
     products = results;
-  } else if (currentQuery) {
-    const { results } = await fetchProdutcsByQuery(currentQuery);
+  } else if (query) {
+    const { results } = await fetchProdutcsByQuery(query);
     products = results;
   }
 
-  // Ordenar produtos
+  const sortOrder = DOM.sortOrderSelect.value;
   const sortedProducts = sortProducts(products, sortOrder);
 
-  // Limpar a lista de produtos
-  sectionItens.innerHTML = '';
+  DOM.sectionItens.innerHTML = '';
 
-  // Atualizar a lista de produtos com os produtos ordenados
   if (sortedProducts.length === 0) {
     clearProductList();
   } else {
     sortedProducts.forEach((element) => {
-      sectionItens.appendChild(createProductItemElement(element));
+      DOM.sectionItens.appendChild(createProductItemElement(element));
     });
   }
 
   checkProductList();
+  toggleLoading(false);
+};
+
+DOM.sortOrderSelect.addEventListener('change', () => {
+  updateProductList(DOM.selectCategories.value);
 });
 
-formPesquisa.addEventListener('submit', async (event) => {
+DOM.formPesquisa.addEventListener('submit', (event) => {
   event.preventDefault();
-
-  const pesquisaInput = document.querySelector('#pesquisaInput');
-  const query = pesquisaInput.value;
-
-  const { results } = await fetchProdutcsByQuery(query);
-  const products = results;
-
-  productList.innerHTML = ''; // Limpa a lista de produtos
-
-  if (products.length === 0) {
-    clearProductList();
-  } else {
-    products.forEach((element) => {
-      productList.appendChild(createProductItemElement(element));
-    });
-  }
-
-  checkProductList();
+  updateProductList(null, DOM.formPesquisa.querySelector('#pesquisaInput').value);
 });
 
+DOM.selectCategories.addEventListener('change', (event) => {
+  updateProductList(event.target.value);
+});
 
 const showCategories = async () => {
-  const categories = await fetchCategories(); // Função que busca as categorias da API
-  const categoriesList = document.querySelector('.categories-list');
+  const categories = await fetchCategories();
+  const categoriesList = DOM.selectCategories;
+
+  const defaultOption = document.createElement('option');
+  defaultOption.value = '';
+  defaultOption.text = 'Selecione uma categoria';
+  categoriesList.appendChild(defaultOption);
 
   categories.forEach((category) => {
-    const categoryOption = document.createElement('option');
-    categoryOption.value = category.id;
-    categoryOption.innerText = category.name;
-    categoriesList.appendChild(categoryOption);
+    const option = document.createElement('option');
+    option.value = category.id;
+    option.text = category.name;
+    categoriesList.appendChild(option);
   });
 };
 
-showCategories();
+const addToCart = async (event) => {
+  const productId = event.target.parentElement.querySelector('span.item__sku').innerText;
+  const cartItems = DOM.cartLista;
+  const addButton = event.target;
 
-selectCategories.addEventListener('change', async (event) => {
-  const categoryId = event.target.value;
-  const { results } = await fetchProductsByCategory(categoryId);
-  const products = results;
+  // Adiciona o efeito de loading
+  const loadingDiv = document.createElement('div');
+  loadingDiv.className = 'loading';
+  addButton.appendChild(loadingDiv);
 
-  sectionItens.innerHTML = ''; // Limpa a lista de produtos
+  // Checa se o item já está no carrinho
+  const existingCartItem = Array.from(cartItems.children).find(
+    (item) => item.getAttribute('data-sku') === productId
+  );
 
-  if (products.length === 0) {
-    clearProductList();
+  if (existingCartItem) {
+    // Se o item já está no carrinho, atualiza a quantidade
+    const quantitySpan = existingCartItem.querySelector('.cart__item__quantity');
+    const quantity = parseInt(quantitySpan.innerText, 10);
+    quantitySpan.innerText = quantity + 1;
   } else {
-    products.forEach((element) => {
-      sectionItens.appendChild(createProductItemElement(element));
+    // Se o item ainda não está no carrinho, adiciona uma nova li
+    try {
+      const item = await fetchItem(productId);
+      cartItems.appendChild(createCartItemElement(item));
+      saveCartItems(cartItems);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Remove o efeito de loading
+  addButton.removeChild(loadingDiv);
+
+  updateTotalPrice();
+};
+
+DOM.sectionItens.addEventListener('click', (event) => {
+  if (event.target.classList.contains('item__add')) {
+    addToCart(event);
+  }
+});
+
+DOM.btnVazio.addEventListener('click', () => {
+  DOM.cartLista.innerHTML = '';
+  saveCartItems(DOM.cartLista);
+  updateTotalPrice();
+});
+
+const updateTotalPrice = () => {
+  const cartItems = DOM.cartLista;
+  const totalPriceDiv = DOM.precoTotalDiv;
+
+  const totalPrice = Array.from(cartItems.children).reduce((acc, item) => {
+    const price = parseFloat(item.getAttribute('data-price'));
+    const quantity = parseInt(item.querySelector('.cart__item__quantity').innerText, 10);
+    return acc + price * quantity;
+  }, 0);
+
+  totalPriceDiv.innerText = `${totalPrice.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`;
+
+};
+
+function loadCartItems() {
+  const cartItems = getSavedCartItems();
+
+  if (cartItems && cartItems.length > 0) {
+    cartItems.forEach((item) => {
+      DOM.cartLista.appendChild(createCartItemElement(item));
     });
-  }
-  
-  checkProductList();
-});
-const clearProductList = () => {
-  productList.innerHTML = '';
-  productList.insertAdjacentHTML('beforeend', '<div class="empty-message">Nenhum produto encontrado.</div>');
-};
-
-const checkProductList = () => {  
-  if (productList && productList.children.length === 0) {
-    emptyMessage.style.display = 'block';
-  } else {
-    emptyMessage.style.display = 'none';
+    updateTotalPrice();
   }
 };
 
-const carregarRequisicao = () => {
-  const elemento = document.createElement('span');
-  elemento.className = 'loading';
-  /* elemento.innerText = 'carregando...'; */
-  carregandoContainer.appendChild(elemento);
-};
-
-const deletaCarregamento = () => {
-  carregandoContainer.innerHTML = null;
-};
-
-const createProductImageElement = (imageSource) => {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-};
-
-const atualizaPreco = () => {
-  const items = document.querySelectorAll('.cart__item');
-  const itemsArray = Array.from(items);
-  const precoTotal = itemsArray.reduce((e, valor) => e + Number(valor.innerText.split('$')[1]), 0);
-  precoTotalDiv.innerText = precoTotal;
-};
-
-const atualizaLocal = (item) => {
-  if (localStorage.cartItems) {
-    const saveLocalStorage = JSON.parse(getSavedCartItems());
-    saveLocalStorage.push(item);
-    saveCartItems(JSON.stringify(saveLocalStorage));
-    return;
-  }
-  saveCartItems(JSON.stringify([item]));
-
-  
-};
-
-const cartItemClick = (item) => {
-  item.target.remove();
-  const totalItems = JSON.parse(getSavedCartItems());
-  const index = totalItems.indexOf(item);
-  totalItems.splice(index, 1);
-  saveCartItems(JSON.stringify(totalItems));
-  atualizaPreco();
-};
-
-const createCartItemElement = ({ id, title, price }) => {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `ID: ${id} | TITLE: ${title} | PRICE: $${price}`;
-  li.addEventListener('click', cartItemClick);
-  return li;
-};
-
-const recuperaLocal = () => {
-  const saveLocalStorage = JSON.parse(getSavedCartItems());
-  saveLocalStorage.forEach((element) => {
-    cartLista.appendChild(createCartItemElement(element));
-  });
-};
-
-btnVazio.addEventListener('click', () => {
-  cartLista.innerHTML = null;
-  localStorage.removeItem('cartItems');
-});
-
-const createCustomElement = (element, className, innerText) => {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-};
-
-const pegaId = ({ target }) => {
-  const id = target.parentNode.firstChild.innerText;
-  fetchItem(id).then((e) => {
-    cartLista.appendChild(createCartItemElement(e));
-    atualizaPreco();
-    atualizaLocal(e);
-    carregarRequisicao();
-  });
-  deletaCarregamento();
-};
-
-const createProductItemElement = ({ id, title, thumbnail }) => {
+function createProductItemElement({ id: sku, title: name, price, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
-  section.appendChild(createCustomElement('span', 'item_id', id));
-  section.appendChild(createCustomElement('span', 'item__title', title));
-  section.appendChild(createProductImageElement(thumbnail));
-  
-  const btnAdicionar = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
-  btnAdicionar.addEventListener('click', pegaId);
+  const button = document.createElement('button');
+  button.className = 'item__add';
+  button.innerText = 'Adicionar ao carrinho!';
 
-  section.appendChild(btnAdicionar);
+  const span = document.createElement('span');
+  span.className = 'item__sku';
+  span.innerText = sku;
+
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = image;
+
+  const p1 = document.createElement('p');
+  p1.className = 'item__title';
+  p1.innerText = name;
+
+  const p2 = document.createElement('p');
+  p2.className = 'item__price';
+  p2.innerText = `R$ ${price.toFixed(2)}`;
+
+  section.appendChild(button);
+  section.appendChild(span);
+  section.appendChild(img);
+  section.appendChild(p1);
+  section.appendChild(p2);
 
   return section;
 };
 
-const adicionaListeners = () => {
-  const adicionarNoCar = document.querySelectorAll('.cart__item');
-  adicionarNoCar.forEach((element) => element.addEventListener('click', cartItemClick));
+function createCartItemElement({ id: sku, title: name, price, thumbnail: image }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.setAttribute('data-sku', sku);
+  li.setAttribute('data-price', price.toFixed(2));
+
+  const span = document.createElement('span');
+  span.className = 'cart__item__price';
+  span.innerText = `R$ ${price.toFixed(2)}`;
+
+  const img = document.createElement('img');
+  img.className = 'cart__item__image';
+  img.src = image;
+
+  const p = document.createElement('p');
+  p.className = 'cart__item__title';
+  p.innerText = name;
+
+  // Adicione controles de quantidade
+  const quantityControls = document.createElement('div');
+  quantityControls.className = 'quantity-controls';
+
+  const decrementBtn = document.createElement('button');
+  decrementBtn.innerHTML = '-';
+  decrementBtn.setAttribute('data-action', 'decrement');
+
+  const quantitySpan = document.createElement('span');
+  quantitySpan.innerText = '1';
+  quantitySpan.className = 'cart__item__quantity';
+
+  const incrementBtn = document.createElement('button');
+  incrementBtn.innerHTML = '+';
+  incrementBtn.setAttribute('data-action', 'increment');
+
+  quantityControls.appendChild(decrementBtn);
+  quantityControls.appendChild(quantitySpan);
+  quantityControls.appendChild(incrementBtn);
+
+  // Crie o botão de exclusão e adicione a imagem da lixeira
+  const deleteBtn = document.createElement('button');
+  deleteBtn.className = 'cart__item__delete';
+  deleteBtn.className = 'cart__item__delete';
+  deleteBtn.innerHTML = '<i class="material-icons">delete</i>';
+
+  li.appendChild(img);
+  li.appendChild(p);
+  li.appendChild(quantityControls); // Adicione os controles de quantidade
+  li.appendChild(span);
+  li.appendChild(deleteBtn);
+
+  return li;
 };
-/* const getIdFromProductItem = (product) => product.querySelector('span.id').innerText; */
 
-
-window.onload = async () => {
-  carregarRequisicao();
-  const categories = await fetchCategories();
-  const firstCategory = categories[0].id;
-  const { results } = await fetchProductsByCategory(firstCategory);
-  const products = results;
-
-  if (products.length === 0) {
-    clearProductList();
-  } else {
-    products.forEach((element) => {
-      sectionItens.appendChild(createProductItemElement(element));
-    });
-  } 
-
-    deletaCarregamento();
-
-  if (localStorage.cartItems) {
-    recuperaLocal();
+DOM.cartLista.addEventListener('click', (event) => {
+  if (event.target.classList.contains('cart__item__delete') || event.target.parentElement.classList.contains('cart__item__delete')) {
+    const cartItem = event.target.closest('.cart__item');
+    cartItem.remove();
+    saveCartItems(DOM.cartLista);
+    updateTotalPrice();
   }
-  atualizaPreco();
-  adicionaListeners();
+});
 
-  checkProductList();
+const updateCartItemQuantity = (element, action) => {
+  const quantitySpan = element.querySelector('.cart__item__quantity');
+  let quantity = parseInt(quantitySpan.innerText, 10);
+
+  if (action === 'increment') {
+    quantity += 1;
+  } else if (action === 'decrement' && quantity > 1) {
+    quantity -= 1;
+  }
+
+  quantitySpan.innerText = quantity;
+
+  const price = parseFloat(element.getAttribute('data-price'));
+  const updatedPrice = price * quantity;
+  element.querySelector('.cart__item__price').innerText = `R$ ${updatedPrice.toFixed(2)}`;
 };
+
+DOM.cartLista.addEventListener('click', (event) => {
+  const action = event.target.getAttribute('data-action');
+  const cartItem = event.target.closest('.cart__item');
+
+  if (action === 'increment' || action === 'decrement') {
+    updateCartItemQuantity(cartItem, action);
+    saveCartItems(DOM.cartLista);
+    updateTotalPrice();
+  }
+});
+
+window.onload = () => {
+  loadCartItems();
+  showCategories();
+  updateProductList();
+};    
